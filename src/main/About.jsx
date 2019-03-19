14518c8c19cc52ef83cc4b88950bd20ab2f5c62f
darya <daryanovak18@gmail.com>
2019-03-19 05:01:42 +0300

    feat: add new footer

--- src/main/About.jsx
+++ src/main/About.jsx
@@ -9,16 +9,16 @@ export class About extends React.Component {
                 <ContentContainer className={css.container}>
                     <div className={css.aboutTextWrapper}>
                         <h2 className={css.aboutTextTitle}>
-                        Quintessentially Flowers - Luxury Florists in London
+                            Quintessentially Flowers - Luxury Florists in London
                     </h2>
                         <p className={css.aboutText}>
-                        <p>Quintessentially Flowers are seasoned experts in choosing the freshest luxury flowers at the daily London markets. Each experienced florist, personally prepares & designs each luxury flower arrangement, ensuring it has just the right balance, shape, colour, scent and elegance.</p>
-                        <br/>
+                            <p>Quintessentially Flowers are seasoned experts in choosing the freshest luxury flowers at the daily London markets. Each experienced florist, personally prepares & designs each luxury flower arrangement, ensuring it has just the right balance, shape, colour, scent and elegance.</p>
+                            <br />
 
-Our Head Florist and Director is Monika Parafianowicz who after completing art college established herself in the London floristry scene very quickly. She has successfully graduated from an excellent two year UK floristry course and worked with many well known flower designers over the years, where she gained a wealth of experience.
-
-Dorota Hassall is the senior florist and director at Quintessentially Flowers.  She qualified as an English teacher but decided to follow her one true passion after moving to London.  Dorota has worked with many of the most respected floral designers in the UK & London and successfully qualified as a florist after a two year extensive floristry course in London.
-                </p>
+                            <p>Our Head Florist and Director is Monika Parafianowicz who after completing art college established herself in the London floristry scene very quickly. She has successfully graduated from an excellent two year UK floristry course and worked with many well known flower designers over the years, where she gained a wealth of experience.
+                            
+Dorota Hassall is the senior florist and director at Quintessentially Flowers.  She qualified as an English teacher but decided to follow her one true passion after moving to London.  Dorota has worked with many of the most respected floral designers in the UK & London and successfully qualified as a florist after a two year extensive floristry course in London.</p>
+                        </p>
                     </div>
                 </ContentContainer>
             </section>
--- src/main/Contacts.jsx
+++ src/main/Contacts.jsx
@@ -9,50 +9,96 @@ import { ContentContainer } from '../common/ContentContainer';
 export class Contacts extends React.Component {
     render() {
         return (
-            <section className={ css.contacts }>
-                <ContentContainer className={ css.container }>
-                    <h2 className={ css.contactsHeader } >Контакты</h2>
-                    <div className={ css.contactsWrapper }>
-                        <div className={ css.communication }>
-                            <div className={ css.communicationContent }>
-                                <h4>Телефон</h4>
-                                <a href="tel:+3752888563803">+375 (29) 6563803</a>
-                                <h4>Email</h4>
-                                <a href="mailto:ignashevich@tut.by">4ch@tut.by</a>
-                                <h4>Skype</h4>
-                                <a href="skype:ignashevich3?chat">yyeyeyey3</a>
-                                <h4>Время работы</h4>
-                                <p>08:00 - 22:00 (Минск)</p>
-                                <h4>Мы поддерживаем</h4>
-                                <div className={ css.communicationSupport }>
-                                    <a href="viber://contact?number=%2B375296563803">
-                                        <FontAwesomeIcon icon={ faViber } />
-                                    </a>
-                                    <a href="whatsapp://send?phone=+375296563803" data-action="share/whatsapp/share">
-                                        <FontAwesomeIcon icon={ faWhatsappSquare } />
-                                    </a>
-                                    <a href="skype:ignashevich3?chat">
-                                        <FontAwesomeIcon icon={ faSkype } />
-                                    </a>
+            <section className={css.contacts}>
+                <ContentContainer className={css.container}>
+                    <div className={css.contactsWrapper}>
+                        <div className={css.communication}>
+                            <div className={css.flowerstation}>
+                                <div className={css.article}>
+                                    <img className={css.image} src="'../../public/images/icon.png" alt="" />
+                                    <h4>FLOWER STATION</h4>
                                 </div>
-                                
+                                <p>
+                                    Fresh Flowers Delivered: <br/>
+                                      Our shop is open 24 hours a day.
+                                </p>
 
                             </div>
-                        </div>
-                        <div className={ css.location }>
-                            <div className={ css.locationContent }>
-                                <address className={ css.locationAdress }>
-                                    <p>г. Лалал, ул. Богдановича 57</p>
-                                </address>
-                                <iframe
-                                    className={ css.map }
-                                    frameBorder="0" style={ { border: 0 } }
-                                    src="https://maps.google.com/maps?q=52.143050,26.028132&hl=es;z=18&amp;output=embed"
-                                    allowFullScreen
-                                    >
-                                </iframe>
+                            <div className={css.getintouch}>
+                            <div className={css.article}>
+                                    <img className={css.image} src="'../../public/images/location.jpg" alt="" />
+                                    <h4>GET IN TOUCH WITH US</h4>
+                                </div>
+                              
+
+                                <p>55 PARK ROAD, LONDON, NW1 6XU
+                                    <br/>
+                                    PHONE: +44 207 724 7525
+                                    <br/>
+                                        FLOWERSTATION.CO.UK</p>
                             </div>
+
+                            {/* <div className={css.location}>
+                                <div className={css.locationContent}>
+                                <h4>BROOKLYN</h4>
+                                
+                                    <iframe
+                                        className={css.map}
+                                        frameBorder="0" style={{ border: 0 }}
+                                        src="https://maps.google.com/maps?q=52.143050,26.028132&hl=es;z=18&amp;output=embed"
+                                        allowFullScreen
+                                    >
+                                    </iframe>
+                                </div>
+                            </div> */}
+
+                            <div className={css.communicationSupport}>
+                            <div className={css.article}>
+                                    <img className={css.image} src="'../../public/images/contacts.png" alt="" />
+                                    <h4>CONTACTS</h4>
+                                </div>
+                            <div  className={css.communicationLinks}>
+                            <div>
+                                <p>VIBER: </p>
+                                <a href="viber://contact?number=%2B375296563803">
+                                    <FontAwesomeIcon icon={faViber} />
+                                </a>
+                            </div>
+                            <div>
+                                <p>WHATAPP: </p>
+                                <a href="whatsapp://send?phone=+375296563803" data-action="share/whatsapp/share">
+                                    <FontAwesomeIcon icon={faWhatsappSquare} />
+                                </a>
+                            </div>
+                            <div>
+                                <p>SKYPE: </p>
+                                <a href="skype:ignashevich3?chat">
+                                    <FontAwesomeIcon icon={faSkype} />
+                                </a>
+                            </div>
+                           
+                              
+                               
+                            </div>
+                            </div>
+
+                            {/* <div className={css.communicationSupportt}>
+                                <h4>Мы поддерживаем</h4>
+                                <a href="viber://contact?number=%2B375296563803">
+                                    <FontAwesomeIcon icon={faViber} />
+                                </a>
+                                <a href="whatsapp://send?phone=+375296563803" data-action="share/whatsapp/share">
+                                    <FontAwesomeIcon icon={faWhatsappSquare} />
+                                </a>
+                                <a href="skype:ignashevich3?chat">
+                                    <FontAwesomeIcon icon={faSkype} />
+                                </a>
+
+
+
+                            </div> */}
                         </div>
+
                     </div>
                 </ContentContainer>
             </section>
